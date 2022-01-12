16fcc18a1d468de9fa3643d847bb7e21e18bf74b
Immanuel Frenzel <imifrenzel@gmail.com>
2022-01-12 10:26:03 +0100

    added missing dates, removed false timestamps, added reimport test, code complete

--- 1_Exercise.R
+++ 1_Exercise.R
@@ -4,6 +4,7 @@
 
 library("tidyverse")
 library("lubridate")
+library("tibbletime")
 
 rm(list = ls(all.names = TRUE))
 
@@ -11,16 +12,27 @@ rawdata <- read.csv("https://raw.githubusercontent.com/data-hydenv/data/master/h
   as_tibble() %>% 
   select(1:4)
 
-rawdata <- rawdata %>% rename("id" = names(rawdata)[1]) %>% 
+rawdata <- rawdata %>%
   rename("charDTTM" = names(rawdata)[2]) %>% 
   rename("temp" = names(rawdata)[3]) %>% 
   rename("lux" = names(rawdata)[4])
 
 rawdata <- rawdata %>% 
-  mutate("posDTTM" = parse_date_time(rawdata$charDTTM, "%d/%m/%Y %H:%M:%S"))
+  mutate(posDTTM = ymd_hms(parse_date_time(rawdata$charDTTM, "%d/%m/%Y %H:%M:%S")))
 
-rawdata <- rawdata %>%
-  mutate("dttm" = format(as.POSIXct(rawdata$posDTTM), "%Y-%m-%d %H:%M"))
+rawdata <- filter_time(tbl_time(rawdata, posDTTM), '2021-12-13 00:00:00' ~ '2022-01-09 10:30:00') 
 
-rawdata <- rawdata %>%
-  select("id" , "dttm", "temp", "lux")
+rawdata <- rawdata %>% 
+  add_row(posDTTM = seq(ymd_hms('2022-01-09 10:40:00'), ymd_hms('2022-01-09 23:00:00'),
+                        by = '10 mins'))
+export <- rawdata %>%
+  mutate("dttm" = format(as.POSIXct(rawdata$posDTTM), "%Y-%m-%d %H:%M"), id = 1:length(rawdata$posDTTM)) %>% 
+  select("id", "dttm", "temp", "lux")
+
+write.csv(export, file = "C:/Users/Imifr/Documents/Github/hyd_data_management/10610854.csv", append = FALSE, quote = FALSE, sep = ",",
+          eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+          col.names = TRUE, qmethod = c("escape", "double"),
+          fileEncoding = "")
+#Tests
+reimport <- read.csv("C:/Users/Imifr/Documents/Github/hyd_data_management/10610854.csv")
+
