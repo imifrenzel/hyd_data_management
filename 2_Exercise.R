8c079813e961ee5f98a042ce907940319bd36cd8
Immanuel Frenzel <imifrenzel@gmail.com>
2022-01-13 14:17:32 +0100

    bugfixes for na values, evaluatin of the qcps

--- 2_Exercise.R
+++ 2_Exercise.R
@@ -15,15 +15,35 @@ data <- reimport %>%
 
 #QCPs
 
+#QCP_1
 data <- data %>%
-  mutate(QCP_1 = ifelse(temp >= -20 && temp <= 70, 1, 0)) %>% 
-  mutate(QCP_2 = ifelse(between(temp - lag(temp), -1, 1), 1, 0)) %>% 
+  mutate(QCP_1 = if_else(temp <= -20 | temp >= 70, 0, 1))
+
+sum <- summarise(data, QCP_1 = sum(QCP_1, na.rm = TRUE))
+length(data$QCP_1) - sum
+summary(data)
+
+#QCP_2
+data <- data %>%
+  mutate(QCP_2 = ifelse(between(temp - lag(temp), -1, 1), 1, 0))
+
+sum2 <- summarise(data, QCP_2 = sum(QCP_2, na.rm = TRUE))
+length(data$QCP_2) - sum2
+summary(data)
+
+#QCP_3
+data <- data %>%
   mutate(QCP_3 = ifelse((temp == lag(temp)) +
                         (temp == lag(temp, n = 2)) +
                         (temp == lag(temp, n = 3)) +
                         (temp == lag(temp, n = 4)) +
                         (temp == lag(temp, n = 5)) == 5, 0, 1 
-                        )) %>% 
+                        )) 
+
+filter(data, QCP_3 == 0)
+
+#QCP_4
+data <- data %>%
   mutate(SIC = case_when(lux < 0 ~ "NA",
                          lux < 10 ~ "night",
                          lux < 500 ~ "sun_rise_or_set",
@@ -32,7 +52,8 @@ data <- data %>%
                          lux < 20000 ~ "clear_sky_shady",
                          lux < 50000 ~ "sunshine",
                          lux >= 50000 ~ "sunshine_bright")) %>% 
-  mutate(QCP_4 = case_when((hour(dttm) < 6 | hour(dttm) >= 18) ~ 1, #set all QCP_4 values for nighttime to 1
+  mutate(QCP_4 = case_when(is.na(SIC) ~ as.double(NA),
+                           hour(dttm) < 6 | hour(dttm) >= 18 ~ 1, #set all QCP_4 values for nighttime to 1
                            lag(SIC, n = 3) == "sunshine_bright" ~ 0, #set 0 if sunshine_bright +- 3 before or ahead
                            lag(SIC, n = 2) == "sunshine_bright" ~ 0,
                            lag(SIC) == "sunshine_bright" ~ 0,
@@ -44,24 +65,30 @@ data <- data %>%
                            SIC == "sunshine" ~ 0,
                            lead(SIC) == "sunshine" ~ 0,
                            TRUE ~ 1)) 
-data_hourly <- data %>% 
-  mutate(QCP_total = ifelse(QCP_1 + QCP_2 + QCP_3 + QCP_4 < 4, 0, 1)) %>% 
+#plot of SIC
+summary(data)
+ggplot(data, mapping = aes(x = SIC)) +
+  geom_bar() +
+  theme_bw()
+
+#plot for QCP counts
+a <- data %>% 
+  summarise_at(vars(QCP_1 , QCP_2, QCP_3, QCP_4), ~ sum(.x == 0, na.rm = TRUE)) %>% 
+  pivot_longer(cols = QCP_1:QCP_4)
+
+ggplot(a, mapping = aes(x = name, y = value)) +
+  geom_col() +
+  theme_bw()
+
+#flagging
+qc_df <- data %>% 
+  mutate(QCP_total = ifelse(QCP_1 + QCP_2 + QCP_3 + QCP_4 < 4, 0, 1))
+
+#aggregate hourly
+hobo_hourly <- qc_df %>% 
   mutate(hour = cut(dttm, breaks = "hour")) %>% 
   group_by(hour) %>% 
-  summarise(date_time = first(hour), th = ifelse(sum(QCP_total) < 6, NA, mean(temp)))
- 
+  summarise(date_time = first(hour), th = round(ifelse(sum(QCP_total) < 6, NA, mean(temp)), digits = 3)) %>% 
+  select("date_time", "th")
 
-
-
-#test 4 QCPS
-filter(data, temp <= -20 && temp >= 70)
-filter(data, !between(temp - lag(temp), -1, 1))
-filter(data, (temp == lag(temp)) +
-         (temp == lag(temp, n = 2)) +
-         (temp == lag(temp, n = 3)) +
-         (temp == lag(temp, n = 4)) +
-         (temp == lag(temp, n = 5)) == 5)
-filter(data, SIC == "sunshine_bright" | SIC == "sunshine")
-
-tinytex::install_tinytex()
 
